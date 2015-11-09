e43200ef9a98c62b0d26a2cb78fdd5217067ec00
kristian@purestyle.se <kristian@purestyle.se>
2015-11-09 20:53:45 +0100

    reading char

--- src/main.c
+++ src/main.c
@@ -3,5 +3,21 @@
 int main() {
 
   printf("Hello pacworld!\n");
+  int something;
+
+  for(;;) {
+
+    something = getchar();
+
+    printf("You entered: ");
+    putchar(something);
+
+    if(something == 'q') {
+
+      printf("Good bye!");
+      break;
+    }
+  }
+
   return 0;
 }
