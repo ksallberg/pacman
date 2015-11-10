573f91b56e91162c9995db1fd40f76a0f6e0bcd8
kristian@purestyle.se <kristian@purestyle.se>
2015-11-10 23:07:47 +0100

    Big character background

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 int pacx = 0;
 int pacy = 0;
 
+int width = 50;
+int height = 25;
+
 #define ANSI_COLOR_RED    "\x1b[31m"
 #define ANSI_COLOR_CYAN   "\x1b[36m"
 #define ANSI_COLOR_YELLOW "\x1b[0;93m"
@@ -16,18 +19,18 @@ void clear_scene() {
 void draw_scene() {
   int i, j = 0;
 
-  for(i = 0; i < 10; i ++) {
-    for(j = 0; j < 20; j ++) {
+  for(i = 0; i < height; i ++) {
+    for(j = 0; j < width; j ++) {
       if(j == pacx && i == pacy) {
         printf(ANSI_COLOR_YELLOW "o" ANSI_COLOR_RESET);
       } else {
-        printf(ANSI_COLOR_CYAN   "x" ANSI_COLOR_RESET);
+        printf(ANSI_COLOR_CYAN "\u2588" ANSI_COLOR_RESET);
       }
     }
     printf("\n");
   }
 
-  if(pacx == 9) {
+  if(pacx == height - 1) {
     pacx = pacy = 0;
   } else {
     pacx ++;
