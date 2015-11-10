1b18a5566a0308118bc39a3ff894f440fc344f98
kristian@purestyle.se <kristian@purestyle.se>
2015-11-10 23:01:32 +0100

    yellow pacman

--- src/main.c
+++ src/main.c
@@ -4,9 +4,10 @@
 int pacx = 0;
 int pacy = 0;
 
-#define ANSI_COLOR_RED   "\x1b[31m"
-#define ANSI_COLOR_CYAN  "\x1b[36m"
-#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_RED    "\x1b[31m"
+#define ANSI_COLOR_CYAN   "\x1b[36m"
+#define ANSI_COLOR_YELLOW "\x1b[0;93m"
+#define ANSI_COLOR_RESET  "\x1b[0m"
 
 void clear_scene() {
   printf("\e[1;1H\e[2J");
@@ -18,9 +19,9 @@ void draw_scene() {
   for(i = 0; i < 10; i ++) {
     for(j = 0; j < 20; j ++) {
       if(j == pacx && i == pacy) {
-        printf(ANSI_COLOR_RED "o" ANSI_COLOR_RESET "");
+        printf(ANSI_COLOR_YELLOW "o" ANSI_COLOR_RESET);
       } else {
-        printf(ANSI_COLOR_CYAN "x" ANSI_COLOR_RESET "");
+        printf(ANSI_COLOR_CYAN   "x" ANSI_COLOR_RESET);
       }
     }
     printf("\n");
