7b42b663e6083fea8a1ec110e175ba405ad115c1
kristian@purestyle.se <kristian@purestyle.se>
2015-11-14 11:55:52 +0100

    added rastafari colors to the walls

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int width = 50;
 int height = 25;
 
 #define ANSI_COLOR_RED    "\x1b[31m"
+#define ANSI_COLOR_GREEN  "\x1b[32m"
 #define ANSI_COLOR_CYAN   "\x1b[36m"
 #define ANSI_COLOR_YELLOW "\x1b[0;93m"
 #define ANSI_COLOR_RESET  "\x1b[0m"
@@ -17,9 +18,15 @@ void clear_scene() {
 }
 
 void test() {
-  printf("╔══════════════╗        ╔══╗\n");
-  printf("║              ╠════════╣  ║\n");
-  printf("╚══════════════╝        ╚══╝\n");
+  printf(ANSI_COLOR_YELLOW
+         "╔══════════════╗        ╔══╗\n"
+         ANSI_COLOR_RESET);
+  printf(ANSI_COLOR_RED
+         "║              ╠════════╣  ║\n"
+         ANSI_COLOR_RESET);
+  printf(ANSI_COLOR_GREEN
+         "╚══════════════╝        ╚══╝\n"
+         ANSI_COLOR_RESET);
 }
 
 void draw_scene() {
