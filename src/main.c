c2ea82982bed21642b61fd19680ce9dd17595d24
kristian@purestyle.se <kristian@purestyle.se>
2015-11-15 15:47:02 +0100

    Added new map and struct for ghosts

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 int pacx = 0;
 int pacy = 0;
 
-int width = 50;
-int height = 25;
+int width  = 19;
+int height = 19;
 
 #define ANSI_COLOR_RED    "\x1b[31m"
 #define ANSI_COLOR_GREEN  "\x1b[32m"
@@ -13,22 +13,61 @@ int height = 25;
 #define ANSI_COLOR_YELLOW "\x1b[0;93m"
 #define ANSI_COLOR_RESET  "\x1b[0m"
 
+/* char *m[] = {"         █         ", */
+/*              " ███ ███ █ ███ ███ ", */
+/*              "                   ", */
+/*              " ███ █ █████ █ ███ ", */
+/*              "     █   █   █     ", */
+/*              "████ ███ █ ███ ████", */
+/*              "████ █       █ ████", */
+/*              "████ █ ██ ██ █ ████", */
+/*              "       █   █       ", */
+/*              "████ █ █████ █ ████", */
+/*              "████ █       █ ████", */
+/*              "████ █ █████ █ ████", */
+/*              "         █         ", */
+/*              " ███ ███ █ ███ ███ ", */
+/*              "   █           █   ", */
+/*              "██ █ █ █████ █ █ ██", */
+/*              "     █   █   █     ", */
+/*              " ███████ █ ███████ ", */
+/*              "                   "}; */
+
+char *m[] = {"         |         ",
+             " ||| ||| | ||| ||| ",
+             "                   ",
+             " ||| | ||||| | ||| ",
+             "     |   |   |     ",
+             "|||| ||| | ||| ||||",
+             "|||| |       | ||||",
+             "|||| | || || | ||||",
+             "       |   |       ",
+             "|||| | ||||| | ||||",
+             "|||| |       | ||||",
+             "|||| | ||||| | ||||",
+             "         |         ",
+             " ||| ||| | ||| ||| ",
+             "   |           |   ",
+             "|| | | ||||| | | ||",
+             "     |   |   |     ",
+             " ||||||| | ||||||| ",
+             "                   "};
+
+struct ghost {
+  char* color;
+  int x;
+  int y;
+  int state; // 1 = hunting, 2 = fleeing, 3 = dead
+};
+
 void clear_scene() {
   printf("\e[1;1H\e[2J");
 }
 
 void test() {
-  const char *m[] = {"╔══════════════╗        ╔══╗",
-                     "║              ╠════════╣  ║",
-                     "╚══════════════╝        ╚══╝",
-                     "                            ",
-                     "       ╔══════════════╗     ",
-                     "       ║              ║     ",
-                     "       ╚══════════════╝     "
-                    };
 
-  int i;
-  for(i = 0; i <= 6; i ++) {
+  int i = 0;
+  for(; i < 19; i ++) {
     printf("%s\n", m[i]);
   }
 }
@@ -41,7 +80,8 @@ void draw_scene() {
       if(j == pacx && i == pacy) {
         printf(ANSI_COLOR_YELLOW "o" ANSI_COLOR_RESET);
       } else {
-        printf(ANSI_COLOR_CYAN "\u2588" ANSI_COLOR_RESET);
+        char str = m[i][j];
+        printf("%c", str);
       }
     }
     printf("\n");
@@ -68,7 +108,7 @@ int main() {
 
     clear_scene();
     draw_scene();
-    test();
+    //    test();
   }
 
   return 0;
